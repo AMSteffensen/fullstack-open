95e8760156489310ddc3dd074e1c14f3933e1fe2
Andreas Steffensen <andreas.steffensen89@gmail.com>
2022-09-01 21:07:25 +0000

    add part 1.6 unicaffe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGoodByOne = () => setGood(good + 1)
+  const increaseNeutralByOne = () => setNeutral(neutral + 1)
+  const increaseBadByOne = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={increaseGoodByOne}>Good</button>   
+      <button onClick={increaseNeutralByOne}>Neutral</button>
+      <button onClick={increaseBadByOne}>Bad</button>
+
+      <h2>Stats</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
